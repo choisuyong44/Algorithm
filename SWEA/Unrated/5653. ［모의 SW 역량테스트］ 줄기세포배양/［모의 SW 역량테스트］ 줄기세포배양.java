8d6f1a3627e833199053548b78d8607682ca9f83
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-08-18 18:42:09 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 357 ms, Memory: 51,948 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -1,8 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+import javax.swing.CellEditor;
+
 public class Solution {
 
+	static final int DEAD = 0;
+	static final int ACTIVE = 1;
+	static final int DEACTIVE = 2;
+
+	static int dr[] = { -1, 1, 0, 0 };
+	static int dc[] = { 0, 0, -1, 1 };
+
+	static int N, M, K;
+
+	static boolean[][] visit;
+
+	static List<Cell> list;
 	static PriorityQueue<Cell> pq = new PriorityQueue<Cell>(new Comparator<Cell>() {
 		@Override
 		public int compare(Cell o1, Cell o2) {
@@ -10,137 +24,101 @@ public class Solution {
 		};
 	});
 
-	static List<Cell> list;
-	
-	static boolean[][] visit;
-	
-	static int N;
-	static int M;
-	static int K;
-	
-	static final int DEAD = 0;
-	static final int ACTIVE = 1;
-	static final int DEACTIVE = 2;
-	
-	static final int[] dr = {-1,1,0,0};
-	static final int[] dc = {0,0,-1,1};
-	
-	static int nr,nc;
-	static Cell temp;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+		StringBuilder sb = new StringBuilder();
+
 		int TC = Integer.parseInt(br.readLine());
-		
-		for(int T = 1; T< TC+1;T++) {
+
+		for (int T = 1; T < TC + 1; T++) {
 			pq.clear();
+			
 			st = new StringTokenizer(br.readLine());
-			
-			list = new ArrayList<Cell>();
-			
+
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
+
+			visit = new boolean[N + K + 1][M + K + 1];
+			list = new ArrayList<Cell>();
 			
-			int ROW = N+(K*2);
-			int COL = M+(K*2);
-			visit = new boolean[ROW][COL];
-			int cell;
-			
-			for(int i =0;i<N;i++) {
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j =0;j<M;j++) {
-					cell = Integer.parseInt(st.nextToken());
-					if(cell > 0) {  
-						// 방문 처리
-						visit[i+K][j+K] = true;
-						// 추가
-						list.add(new Cell((i+K),(j+K),cell));
+				for (int j = 0; j < M; j++) {
+					int k = Integer.parseInt(st.nextToken());
+					if (k > 0) {
+						visit[i + K / 2][j + K / 2] = true;
+						list.add(new Cell(i + K / 2, j + K / 2, k));
 					}
 				}
 			}
-			
-			growth();
-			
-			// list에서 돌면서 갯수 세기
-			int cnt =0;
-			for(Cell c : list) {
-				if(c.status == DEACTIVE || c.status ==ACTIVE) {
+
+			grow();
+
+			int cnt = 0;
+			for (Cell c : list) {
+				if (c.status == ACTIVE || c.status == DEACTIVE) {
 					cnt++;
 				}
 			}
-
-			sb.append("#" +T + " " + cnt +"\n");
+			sb.append("#").append(T).append(" ").append(cnt).append("\n");
 		}
+
 		System.out.println(sb.toString());
 	}
 
-	public static void growth() {
-		while(K-- >= 0) {
-			
-			while(!pq.isEmpty()) {
+	static void grow() {
+
+		while (K-- >= 0) {
+
+			while (!pq.isEmpty()) {
 				Cell tmp = pq.poll();
-				
-				// 넣기
-				for (int d =0;d<4;d++) {
+
+				for (int d = 0; d < 4; d++) {
+					int nr = tmp.r + dr[d];
+					int nc = tmp.c + dc[d];
 					
-					nr = tmp.r + dr[d];
-					nc = tmp.c + dc[d];
-					
-					if(visit[nr][nc] == false) {
-						list.add(new Cell(nr,nc,tmp.ready));
+					if(visit[nr][nc]==false) {
 						visit[nr][nc] = true;
+						list.add(new Cell(nr,nc,tmp.ready));
 					}
 				}
 			}
 			
-			for(Cell c : list) {
+			for(Cell c :list) {
 				c.next();
 			}
 		}
 	}
-	
+
 	static class Cell {
-		int r;
-		int c;
-		
-		// DEACTIVE 대기 시간
-		int ready;
-		int aliveTime;
-		
-		// DEACTIVE
-		int status;
-		
-		// 살아있는 시간을 누적시켜서 더할 예정
-		public Cell(int row, int col, int ready) {
-			this.r = row;
-			this.c = col;
-			this.ready = ready;
-			this.aliveTime = ready*2;
+		int r, c, status, life, ready;
+
+		public Cell(int r, int c, int ready) {
+			this.r = r;
+			this.c = c;
 			this.status = DEACTIVE;
+			this.ready = ready;
+			this.life = ready * 2;
 		}
-		
-		public void next() {
-			if(status ==DEAD) {
+
+		void next() {
+			if (status == DEAD)
 				return;
-			}
-			
-			if(this.aliveTime == this.ready && this.status == DEACTIVE) {
-				aliveTime--;
+
+			else if (life == ready && status == DEACTIVE) {
+				life--;
 				status = ACTIVE;
 				pq.add(this);
 				return;
 			}
-			
-			if(this.aliveTime == 0 && this.status == ACTIVE) {
+
+			else if (life == 0 && status == ACTIVE) {
 				status = DEAD;
-				return;
 			}
-			
-			aliveTime--;
+
+			life--;
 		}
 	}
 }
