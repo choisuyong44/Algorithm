0af9d74abc8b51d46b8a695de75afa64567e95d7
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-08-21 20:15:33 +0900

    [Silver I] Title: 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유, Time: 832 ms, Memory: 329488 KB -BaekjoonHub

--- 백준/Silver/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.java
+++ 백준/Silver/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.java
@@ -1,113 +1,109 @@
 import java.io.*;
-import java.util.*;
-
-class Point {
-	int x;
-	int y;
-	
-	public Point(int row, int col) {
-		this.x = col;
-		this.y = row;
-	}
-}
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
-	static int N;
-	static int M;
-	
-	static char[][] map;
-	
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		int row =0 ;int col =0 ;
-		map = new char[N][M];
-		
-		String s;char c;
-		for(int i=0;i<N;i++) {
-			s = br.readLine();
-			for(int j=0;j<M;j++) {
-				c = s.charAt(j);
-				
-				if(c =='2') {
-					map[i][j] ='0';
-					row= i; col=j;
-				}
-				
-				else if(c =='0') {
-					map[i][j] ='0';
-				}
-				
-				else if(c == '1') {
-					map[i][j] = '.';
-				}
-				
-				else {
-					map[i][j] = '*';
-				}
-			}
-		}
-		
-		int ans = 0;
-		ans = bfs(row,col);
-		
-		if(ans == 0) {
-			System.out.println("NIE");
-		}
-		
-		else {
-			System.out.println("TAK");
-			System.out.println(ans);
-		}
-	}
-	
-	public static int bfs(int row, int col) {
-		Queue<Point> queue = new LinkedList<Point>();
-		
-		queue.offer(new Point(row,col));
-		Point p; int r; int c;
-		while(!queue.isEmpty()) {
-			
-			p = queue.poll();
-			r = p.y; c= p.x;
-			// printMap();
-			
-			for(int d =0;d<4;d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if(nr>=0 && nr< N && nc >=0 && nc <M) {
-					
-					if(map[nr][nc] == '0') {
-						map[nr][nc] = (char)(map[r][c]+1);
-						queue.offer(new Point(nr,nc));
-					}
-					
-					else if(map[nr][nc]== '*') {
-						return (map[r][c]+1 -'0');
-					}
-				}
-			}
-		}
-		return 0;
-	}
-	
-	public static void printMap() {
-		System.out.println("==========================================");
-		for(int r = 0;r<N;r++) {
-			for(int c =0 ;c<M;c++) {
-				System.out.print(map[r][c]);
-			}
-			System.out.println();
-		}
-		System.out.println("==========================================");
-	}
+    static int[][] island;
+    static int startX;
+    static int startY;
+
+    // 상하좌우
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        input();
+        int answer = bfs();
+        print(answer);
+    }
+
+    private static int bfs() {
+        Queue<Node> queue = new LinkedList<>();
+
+        // 딱따구리 시작 위치 입력
+        queue.add(new Node(startY, startX, 1));
+
+        // 시작 위치를 기준으로 탐색 시작
+        // 벽이면 패스, 매 반복마다 카운트를 증가시키고 3,4,5 중에 하나가 나오는 순간 종료
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int y = node.y;
+            int x = node.x;
+            int cnt = node.cnt;
+
+            // 지나온 곳을 되돌아가는 걸 방지하기 위해 현위치를 1로 변경
+
+            for (int direction = 0; direction < 4; direction++) {
+                int nx = x + dx[direction];
+                int ny = y + dy[direction];
+
+                if (nx < 0 || nx >= island[0].length || ny < 0 || ny >= island.length) continue;
+
+                // 음식인 경우
+                if (island[ny][nx] == 3 || island[ny][nx] == 4 || island[ny][nx] == 5) {
+                    return cnt;
+                }
+                // 벽이면 패스
+                if (island[ny][nx] != 1) {
+                    island[ny][nx] = 1;
+                    queue.add(new Node(ny, nx, cnt + 1));
+                }
+            }
+        }
+
+        // 여기 도달했다는 건 음식을 먹을 수 없는 경우
+        return -1;
+    }
+
+    private static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+
+        island = new int[row][col];
+
+        // 섬 정보 입력
+        for (int r = 0; r < row; r++) {
+            String str = br.readLine();
+            for (int c = 0; c < col; c++) {
+                int num = str.charAt(c) - '0';
+                island[r][c] = num;
+                if (num == 2) {
+                    startX = c;
+                    startY = r;
+                }
+            }
+        }
+
+    }
+
+    private static void print(int answer) throws IOException {
+        if (answer == -1) {
+            bw.write("NIE");
+        } else {
+            bw.write("TAK\n");
+            bw.write(Integer.toString(answer));
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+        int y;
+        int x;
+        int cnt;
+
+        public Node(int y, int x, int cnt) {
+            this.y = y;
+            this.x = x;
+            this.cnt = cnt;
+        }
+    }
 }
