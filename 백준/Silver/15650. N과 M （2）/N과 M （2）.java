656689038bb0e0009b81b506f01d1d3622191fa8
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-08-30 17:20:18 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr;
+	static boolean[] visit;
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[M];
+		visit= new boolean[N+1];
+		
+		per(1,0);
+		
+		System.out.println(sb.toString());
+	}
+
+	public static void per(int idx,int cnt) {
+		if(cnt == M) {
+			for(int i=0;i<M;i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = idx;i<=N;i++) {
+			if(visit[i] == false) {
+				visit[i] = true;
+				arr[cnt] = i;
+				per(i+1,cnt+1);
+				visit[i] = false;
+			}
+		}
+	}
+}
