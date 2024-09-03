c96baa49f21c2e6515704e361dc3f0fd5f4c5ef1
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-09-03 14:22:23 +0900

    [Silver II] Title: 섬의 개수, Time: 100 ms, Memory: 13784 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -3,95 +3,70 @@ import java.util.*;
 
 public class Main {
 
-	static Queue<Pos> q = new LinkedList<Pos>();
-	
-	static int[] dr= {-1,-1,0,1,1,1,0,-1};
-	static int[] dc= {0,1,1,1,0,-1,-1,-1};
-	
-	static int ROW,COL;
-	static int[][] map;
+	static Queue<int[]> q = new LinkedList<int[]>();
+
 	static boolean[][] visit;
-	
-	static int cnt;
-	
-	public static void main(String[] args) throws IOException{
+	static int[][] map;
+	static int w, h;
+
+	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1};
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 너비(COL) 높이(ROW)
-		COL = Integer.parseInt(st.nextToken());
-		ROW = Integer.parseInt(st.nextToken());
-		while(ROW !=0 && COL !=0) {
-			
-			// 맵 생성 및 초기화
-			map = new int[ROW][COL];
-			visit = new boolean[ROW][COL];
-			
-			for(int r= 0;r<ROW;r++) {
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		while (w != 0 || h != 0) {
+			map = new int[h][w];
+			visit = new boolean[h][w];
+
+			for (int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int c = 0;c<COL;c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+				for (int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			// for-for BFS -- cnt
-			cnt = 0;
-			for(int r =0;r<ROW;r++) {
-				for(int c =0;c<COL;c++) {
-					if(visit[r][c]==false && map[r][c]==1) {
-						// queue clear
-						q.clear();
+
+			int cnt = 0;
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if (!visit[i][j] && map[i][j] == 1) {
+						visit[i][j] = true;
+						bfs(i, j);
 						cnt++;
-						bfs(r,c,cnt);
 					}
 				}
 			}
-			
-			// 출력
+
 			System.out.println(cnt);
-			
-			// 마지막에 다음을 위한 입력
-			// 너비(COL) 높이(ROW)
+
 			st = new StringTokenizer(br.readLine());
-			COL = Integer.parseInt(st.nextToken());
-			ROW = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
 		}
 	}
-	
-	public static void bfs(int row,int col,int cnt) {
-		// queue에 넣고 방문 처리
-		q.add(new Pos(row,col,cnt));
-		visit[row][col] = true;
-		
-		while(!q.isEmpty()) {
-			
-			Pos k = q.poll();
-			
-			for(int d=0;d<8;d++) {
-				int nr = k.r + dr[d];
-				int nc = k.c + dc[d];
-				
-				if(nr>=0 && nr<ROW && nc>=0 && nc<COL) {
-					if(visit[nr][nc]==false && map[nr][nc]==1) {
-						map[nr][nc] =cnt;
+
+	public static void bfs(int r, int c) {
+		q.add(new int[] { r, c });
+		visit[r][c] = true;
+
+		while (!q.isEmpty()) {
+
+			int[] k = q.poll();
+
+			for (int d = 0; d < 8; d++) {
+				int nr = k[0] + dr[d];
+				int nc = k[1] + dc[d];
+
+				if (nr >= 0 && nr < h && nc >= 0 && nc < w) {
+					if (!visit[nr][nc] && map[nr][nc] == 1) {
+						q.add(new int[] { nr, nc });
 						visit[nr][nc] = true;
-						q.add(new Pos(nr,nc,cnt));
 					}
 				}
 			}
-		}
-		
-	}
-	
-	static class Pos{
-		int r;
-		int c;
-		int cnt;
-		
-		public Pos(int r, int c, int cnt) {
-			this.r = r;
-			this.c = c;
-			this.cnt = cnt;
 		}
 	}
 }
