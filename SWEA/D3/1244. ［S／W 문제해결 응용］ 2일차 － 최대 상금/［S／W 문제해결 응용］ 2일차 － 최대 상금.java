c2d4092a2ed530e2ef9e8e872287e83902d44bf8
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-09-03 15:36:03 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 485 ms, Memory: 20,656 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,71 +1,71 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Solution {
 
-	static int[] num;
 	static int max;
-	static int switchCnt;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void swap(int i1,int i2) {
-		int tmp = num[i1];
-		num[i1] = num[i2];
-		num[i2] = tmp;
-	}
-	
-	public static void main(String[] args) throws IOException{
+	static int cnt;
+	static String s;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		int TC = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
-		String s;
-		
-		for(int t =1;t<T+1;t++) {
-
-			// 입력
+		for (int T = 1; T < TC + 1; T++) {
 			st = new StringTokenizer(br.readLine());
 			s = st.nextToken();
-			num = new int[s.length()];
-			
-			for(int i =0;i<s.length();i++) {
-				num[i] = s.charAt(i)-'0';
+
+			arr = new int[s.length()];
+
+			for (int i = 0; i < s.length(); i++) {
+				arr[i] = s.charAt(i) - '0';
 			}
-			
-			switchCnt = Integer.parseInt(st.nextToken());
-			
-			// 문자열 횟수만 교환이 일어나면 되므로, 나머지는 불필요한 교환 
-			if(switchCnt > s.length()) {
-				switchCnt  = s.length();
-			}
+
+			cnt = Integer.parseInt(st.nextToken());
+
+			// 최적화 -> 각 자리마다 하나씩 배치
+			// s.length()번 이면 충분함
+			if (cnt > s.length())
+				cnt = s.length();
 			
 			max =0;
 			
-			dfs(0,0,s.length());
+			combi(0);
 			
-			sb.append("#").append(t).append(" "+ max+"\n");
-		}
-		System.out.println(sb);
-	}
-	
-	public static void dfs(int idx,int cnt,int len) {
-		if(cnt == switchCnt) {
-			String s = "";
-			for(int i : num) {
-				s+=Integer.toString(i);
-			}
-			
-			max = Math.max(max,Integer.parseInt(s));
-			return;
+			sb.append("#").append(T).append(" ").append(max).append("\n");
 		}
 		
-		for(int i = idx;i<len-1;i++) {
-			for(int j = i+1;j<len;j++) {
+		System.out.println(sb.toString());
+	}
+
+	static void combi(int depth) {
+		if (depth == cnt) {
+			int ans =0; int digit =1;
+			
+			for(int i=s.length()-1;i>=0;i--) {
+				ans += digit*arr[i];
+				digit *= 10;
+			}
+			max = Math.max(ans, max);
+			
+			return;
+		}
+
+		for (int i = 0; i < s.length()-1; i++) {
+			for (int j = i+1 ; j < s.length(); j++) {
 				swap(i,j);
-				dfs(i,cnt+1,len);
+				combi(depth+1);
 				swap(i,j);
 			}
 		}
+	}
+
+	static void swap(int v1, int v2) {
+		int tmp = arr[v1];
+		arr[v1] = arr[v2];
+		arr[v2] = tmp;
 	}
 }
