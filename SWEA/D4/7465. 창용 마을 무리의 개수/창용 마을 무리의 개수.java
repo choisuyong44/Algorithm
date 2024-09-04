80b9ae5a7cc30494ce9d116104869ae8d4da4cbf
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-09-04 12:17:36 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 132 ms, Memory: 24,616 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,61 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 
 	static int[] parent;
-	static int N,M;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		int TC = Integer.parseInt(br.readLine());
-		
-		for(int T=1;T<TC+1;T++) {
+		int N, M;
+
+		for (int T = 1; T < TC + 1; T++) {
+
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			
+			// make-set
 			parent = new int[N+1];
-			for(int i=1;i<N+1;i++) {
-				parent[i] = i;
+			for(int i=1;i<=N;i++) {
+				parent[i] =i;
 			}
 			
+			int a,b;
 			for(int i=0;i<M;i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
 				union(a,b);
 			}
 			
-			
-			Set<Integer> set = new HashSet<>();
-			for(int i=1;i<N+1;i++) {
-				set.add(find(i));
+			// find 로 찾아야함 아직 리뉴얼이 모두 된 것이 아님
+			int cnt =0;
+			for(int i=1;i<=N;i++) {
+				if(find(i)== i) cnt++;
 			}
-
-			sb.append("#").append(T).append(" ").append(set.size()).append("\n");
+			
+			sb.append("#").append(T).append(" ").append(cnt).append("\n");
 		}
+		
 		System.out.println(sb.toString());
 	}
 	
-	public static boolean union(int x, int y) {
-		x = find(x); //x의 부모노드 찾기
-		y = find(y); //y의 부모노드 찾기
-	    
-		// 이미 같은 그래프에 속해있을 때 false 반환
-		if(x == y) return false;
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
 		
-		if(x <= y) parent[y] = x;
-		else parent[x] = y;
-		return true;
+		if(x<y) parent[y] = x;
+		else parent[x] =y;
 	}
 	
 	public static int find(int x) {
-		if(parent[x] == x) return x;
-		return find(parent[x]);
+		if(x == parent[x]) return x;
+		return parent[x] = find(parent[x]);
 	}
 }
