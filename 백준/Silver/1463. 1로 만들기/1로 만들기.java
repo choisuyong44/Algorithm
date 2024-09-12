beccd1d3501a4503dddb6e85cc6c1450c8301a33
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-09-12 11:10:20 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 16832 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,29 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
 		
-		int[] dp = new int[1000001];
-		
 		int N = sc.nextInt();
-		dp[1] =0;
-		dp[2] =1;
-		dp[3] =1;
 		
-		for(int i= 4;i<=1_000_000;i++) {
-			dp[i] = dp[i-1]+1;
-			if(i%2==0) {
-				dp[i] = Math.min(dp[i],dp[i/2]+1);
-			}
-			if(i%3==0) {
-				dp[i] = Math.min(dp[i],dp[i/3]+1);
-			}
+		int[] dp = new int[1_000_001];
+		
+		// 1에서 출발
+		dp[1] = 0;
+		
+		for(int i = 2;i<=N;i++) {
+			// 1을 더하는 경우
+			int min = dp[i-1] +1;
+			
+			// 2를 곱하는 경우
+			if(i%2 ==0) min = Math.min(min, dp[i/2]+1);
+			
+			// 3을 곱하는 경우
+			if(i%3 ==0) min = Math.min(min, dp[i/3]+1);
+		
+			dp[i] = min;
 		}
 		
 		System.out.println(dp[N]);
