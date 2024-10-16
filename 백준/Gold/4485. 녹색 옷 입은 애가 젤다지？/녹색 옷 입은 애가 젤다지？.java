3327b06a1b068d07c9a80b48a1bca0eda4b9467f
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-10-17 08:23:05 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 156 ms, Memory: 18392 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,58 +1,89 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static Queue<int[]> q = new LinkedList<int[]>();
-	static int N,nr,nc;
-	static int INF = Integer.MAX_VALUE;
-	static int[][] map;
-	static int[][] ans;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int tc =0;
-		while(true) {
-			N = Integer.parseInt(br.readLine());
-			if(N==0) break;
-			ans = new int[N][N];
-			map = new int[N][N];
-			for(int i =0;i<N;i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j =0;j<N;j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			// ans 초기화
-			for(int i=0;i<N;i++) {
-				Arrays.fill(ans[i], INF);
-			}
-			bfs(0,0);
-			sb.append("Problem ").append(++tc).append(": ").append(ans[N-1][N-1]).append("\n");
+	static int N;
+	static int[][] map, dist;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static class Node implements Comparable<Node> {
+		int r, c, cost;
+
+		public Node(int r, int c, int cost) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cost = cost;
 		}
-		System.out.println(sb.toString());
+
+		@Override
+		public int compareTo(Node n) {
+			return Integer.compare(this.cost, n.cost);
+		}
 	}
 	
-	public static void bfs(int r,int c) {
-		q.clear(); q.add(new int[] {r,c});
-		ans[r][c] = map[r][c];
-		while(!q.isEmpty()) {
-			int[] k = q.poll();
-			for(int d=0;d<4;d++) {
-				nr = k[0]+dr[d];
-				nc = k[1]+dc[d];
-				if(nr>=0 && nr<N && nc>=0 && nc<N) {
-					if(ans[nr][nc] > ans[k[0]][k[1]]+map[nr][nc]) {
-						ans[nr][nc] = ans[k[0]][k[1]]+map[nr][nc];
-						q.add(new int[] {nr,nc});
-					}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int tc = 1;
+		
+		while(true) {
+			N = Integer.parseInt(br.readLine());
+			
+			if(N == 0) break;
+			
+			map = new int[N][N];
+			for (int r = 0; r < N; r++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int c = 0; c < N; c++) {
+					map[r][c] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			dist = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(dist[i], Integer.MAX_VALUE);
+			}
+			
+			dijkstra();
+			
+			sb.append("Problem " + tc++ + ": " + dist[N - 1][N - 1]).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(0, 0, map[0][0]));
+		dist[0][0] = map[0][0];
+		
+		boolean[][] visit = new boolean[N][N];
+		visit[0][0] = true;
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = cur.r + dr[d];
+				int nc = cur.c + dc[d];
+				
+				if(nr < 0 || nr >= N || nc < 0 || nc >= N || visit[nr][nc]) continue;
+				
+				if(dist[nr][nc] > cur.cost + map[nr][nc]) {
+					dist[nr][nc] = cur.cost + map[nr][nc];
+					visit[nr][nc] = true;
+					
+					pq.offer(new Node(nr, nc, dist[nr][nc]));
 				}
 			}
 		}
-		
 	}
+
 }
