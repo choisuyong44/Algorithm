23fe3698356745e449bc676f77d7c17b34ed1eb2
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-10-21 19:39:07 +0900

    [level 2] Title: 리코쳇 로봇, Time: 38.02 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,84 +1,115 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     
-    static Queue<int[]> q = new LinkedList<>();
-    static boolean[][] visited;
-    static int[][] map;
-    static int[] dr = {-1,1,0,0};
-    static int[] dc = {0,0,-1,1};
-    static int N,M,nr,nc;
-    static int rr,rc,gr,gc;
-    static boolean find;
-    static int cnt;
-    
     public int solution(String[] board) {
-        N = board.length;
-        M = board[0].length();
         
-        visited = new boolean[N][M];
-        map = new int[N][M];
+        int N = board.length;
+        int M = board[0].length();
+        // System.out.println(N);
+        // System.out.println(M);
         
-        // map으로 변환
-        // R Point, G Point 찾기
-        // rr rc gr gc
-        for(int i =0;i<N;i++){
-            for(int j =0;j<M;j++){
-                if(board[i].charAt(j)=='D')map[i][j]=1;
-                else if(board[i].charAt(j)=='R') {
-                    rr = i; rc =j;
-                }
-                else if(board[i].charAt(j)=='G'){
-                    gr= i; gc=j;
+        int startR = 0, startC = 0;
+        int goalR = 0, goalC = 0;
+        char[][] map = new char[N][M];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                map[i][j] = board[i].charAt(j);
+                if(map[i][j] == 'R'){
+                    startR = i;
+                    startC = j;
+                } else if(map[i][j] == 'G'){
+                    goalR = i;
+                    goalC = j;
                 }
             }
         }
         
-        // find cnt answer초기화
-        find = false; cnt =0; int answer =0;
-        bfs(rr,rc);
+        // System.out.println(startR + " " + startC);
         
-        if(!find) answer = -1;
-        else answer = cnt;
-        return answer;
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        
+        boolean isPoss = false;
+        for (int d = 0; d < 4; d++){
+            int nr = goalR + dr[d];
+            int nc = goalC + dc[d];
+            
+            if(nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 'D') {
+                isPoss = true;
+                break;
+            }
+        }
+        
+        if(!isPoss){
+            return -1;
+        }
+        
+        Queue<int[]> q = new LinkedList<>();
+        for (int d = 0; d < 4; d++){
+            int nr = startR + dr[d];
+            int nc = startC + dc[d];
+            
+            if(nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 'D') continue;
+            
+            q.offer(new int[] {nr, nc, d, 0});
+        }
+        
+        // System.out.println(q);
+        boolean[][] visit = new boolean[N][M];
+        visit[startR][startC] = true;
+        int cnt = -1;
+        // boolean canTouch = false;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            
+            int curR = cur[0];
+            int curC = cur[1];
+            int nextR = curR + dr[cur[2]];
+            int nextC = curC + dc[cur[2]];
+
+            while(nextR >= 0 && nextR < N && nextC >= 0 && nextC < M && map[nextR][nextC] != 'D'){
+                curR = nextR;
+                curC = nextC;
+                nextR = curR + dr[cur[2]];
+                nextC = curC + dc[cur[2]];
+            }
+            
+            if(visit[curR][curC]) continue;
+            
+            cur[3]++;
+            if(map[curR][curC] == 'G') {
+                cnt = cur[3];
+                // canTouch = true;
+                break;
+            }
+            
+            System.out.println(cur[3] + ": " + curR + " " + curC);
+            visit[curR][curC] = true;
+            for (int d = 0; d < 4; d++){
+                int nr = curR + dr[d];
+                int nc = curC + dc[d];
+                
+                if(nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 'D' || visit[nr][nc]) continue;
+                
+                q.offer(new int[] {nr, nc, d, cur[3]});
+            }
+        }
+        
+        return cnt;
     }
     
-    public void bfs(int r, int c){
-        q.clear();
-        q.add(new int[]{r,c,0});
-        visited[r][c] = true;
+//     static void move(int r, int c, int d){
+//         int curR = r;
+//         int curC = c;
+//         int nextR = curR + dr[d];
+//         int nextC = curC + dc[d];
         
-        while(!q.isEmpty()){
-            int[] k = q.poll();
-            
-            // k[0]== gr, k[1]==gc -> return
-            if(k[0]==gr && k[1]==gc) {
-                find = true; cnt = k[2]; return;
-            }
-            
-                
-            out: for(int d = 0;d<4;d++){
-                nr = k[0]; nc = k[1];
-                while(true){
-                    nr = nr+dr[d];
-                    nc = nc+dc[d];
-                    // nr >=0 && nr<N && nc>=0 nc<M
-                    // map[nr][nc]!=1
-                    if(nr < 0 || nr>=N || nc<0 ||nc>=M || map[nr][nc]==1){
-                        nr = nr-dr[d];
-                        nc = nc-dc[d];
-                        break;
-                    }
-                }
-                
-                // 만약 방문했다면 Continue
-                if(visited[nr][nc]) continue;
-                
-                // 방문처리 후 queue에 넣어줌
-                visited[nr][nc] = true;
-                q.add(new int[]{nr,nc,k[2]+1});
-            }
-        }
-    }
+//         while(map[nextR][nextC] != 'D'){
+//             curR = nextR;
+//             curC = nextC;
+//             nextR = curR + dr[d];
+//             nextC = curC + dc[d];
+//         }
+//     }
 }
