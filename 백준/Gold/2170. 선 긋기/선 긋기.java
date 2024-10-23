0984a44fa8ed3f615adb802eab0541d926386458
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-10-23 15:23:59 +0900

    [Gold V] Title: 선 긋기, Time: 2992 ms, Memory: 351208 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int N;
+	static PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			if(o1[0]==o2[0]) return o2[1]-o1[1];
+			return o1[0]-o2[0];
+		}
+	});
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for(int i=0;i<N;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a>b) pq.add(new int[] {b,a});
+			else pq.add(new int[] {a,b});
+		}
+		
+		int ans =0;
+		int[] k = pq.poll();
+		int start = k[0]; int end = k[1];
+		while(!pq.isEmpty()) {
+			k = pq.poll();
+			if(k[0] > end) {
+				ans += end-start;
+				start = k[0];
+			}
+			if(k[1] > end) end =k[1];
+
+		}		
+		
+		ans += end-start;
+		
+		System.out.println(ans);
+	}
+}
