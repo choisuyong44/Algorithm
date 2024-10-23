fc36a16a4301421c088f23db93422a0290b362ac
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-10-23 15:29:38 +0900

    [Gold V] Title: 선 긋기, Time: 1376 ms, Memory: 323988 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -7,7 +7,6 @@ public class Main {
 	static PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
 		@Override
 		public int compare(int[] o1, int[] o2) {
-			if(o1[0]==o2[0]) return o2[1]-o1[1];
 			return o1[0]-o2[0];
 		}
 	});
