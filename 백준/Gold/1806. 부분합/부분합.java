3a9551b1d9857963b7674ec0825fc68a7bd3f4e4
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-10-26 15:50:46 +0900

    [Gold IV] Title: 부분합, Time: 208 ms, Memory: 22404 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,39 +1,47 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 1. 처음,마지막 원소가 S 인 경우
+ * 2. 원소가 없는 경우
+ * 3. 
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-
+	// N의 모든 합은 Int 범위
+	static int N, S;
+	static int[] arr;
+	public static void main(String[] args) throws IOException{
+		input();
+		int start = -1; int end =0;
+		int ans = Integer.MAX_VALUE; int sum =arr[end];
+		
+		while(start < end) {
+			if(sum >= S) ans =Math.min(ans, end-start);
+			if((end >= N-1) || ( sum >= S)) { 			// end가 범위를 초과, 또는 sum >= S면 줄여도 됨
+				start++;
+				sum -= arr[start];
+				
+			}
+			else if(sum < S) {							// 조건보다 작은 경우 end 추기
+				end++;
+				sum += arr[end];
+			}
+		}
+		
+		if(ans == Integer.MAX_VALUE) ans =0;
+		System.out.println(ans);
+	}
+	
+	public static void input() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-
-		int[] arr = new int[N+1];
-		int length = Integer.MAX_VALUE;
-
-		// 1만번 -> 1억
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < N; i++) {
+		for(int i =0;i<N;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int sum = 0;
-		int start = 0;
-		int end = 0;
-		int flag = 0;
-		
-        while(start <= N && end <= N) {
-            if(sum >= S && length> end - start) length = end - start;
-            
-            if(sum < S) sum += arr[end++];
-            else sum -= arr[start++];
-        }
-        if(length ==Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(length);
+		}		
 	}
-
 }
