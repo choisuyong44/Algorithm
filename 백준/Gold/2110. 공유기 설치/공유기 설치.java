60f27a06ce8e6da8c4e4794cbcdaeb084cb10503
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-11-01 07:18:14 +0900

    [Gold IV] Title: 공유기 설치, Time: 236 ms, Memory: 27808 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,60 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N,C;
-	static int[] house;
+	static int N, C;
+	static int[] arr;
 	
-	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		C = sc.nextInt();
-		
-		house = new int[N];
-		
-		// 집에 공유기가 하나씩 달려있는 경우가 최소 거리
-		int min = 1;
-		
-		// 첫 집과 가장 끝 집에 달려있는 경우가 최대 거리
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+		N = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		arr = new int[N];
 		for(int i =0;i<N;i++) {
-			house[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		Arrays.sort(house);
-		
-		binarySearch(min, house[N-1]-house[0]+1);
-	}
-	
-	public static void binarySearch(int min,int max) {
-		int mid =0;
+		// Upper Bound 
+		Arrays.sort(arr);
+		int min = 1; int max = arr[N-1]-arr[0]+1;
 		while(min < max) {
-			
-			mid = (min+max)/2;
-			
-			if(countRouter(mid) < C ) {
-				max = mid;
+			int mid = (min+max)/2;
+			int share = findDist(mid);
+			if(share >= C) {
+				 min = mid+1;
 			}
-			
 			else {
-				min = mid+1;
+				max = mid;
 			}
 		}
 		System.out.println(min-1);
 	}
 	
-	public static int countRouter(int dist) {
-		int cnt =1;
-		int base = house[0];
-		
-		for(int i =0;i<house.length;i++) {
-			if(house[i]-base >= dist) {
-				base = house[i];
+	public static int findDist(int dist) {
+		int cnt = 1; int base = arr[0];
+		for(int i =1;i<N;i++) {
+			if(arr[i]-base >= dist) {
+				base = arr[i];
 				cnt++;
 			}
 		}
-		
 		return cnt;
 	}
 }
