056b2c10518a1ecb61364909f3f846e58aa79219
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-11-16 01:17:11 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 76 ms, Memory: 12532 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,62 +1,66 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+
+// 1칸에는 사다리 또는 뱀 하나만 가능
+// 가장 먼저 도착한 게 가장 빠른길
 public class Main {
 
-	static int[] map = new int[101];
-	static int[] visit = new int[101];
-
-	static int min = Integer.MAX_VALUE;
-
-	public static void main(String[] args) throws IOException {
+	static int N,M;
+	static boolean[] visited;
+	static Queue<int[]> q;
+	static int[][] link;
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int ladder = Integer.parseInt(st.nextToken());
-		int snake = Integer.parseInt(st.nextToken());
-
-		for (int i = 1; i < 101; i++) {
-			map[i] = i;
-		}
-
-		int start;
-		int end;
-		for (int i = 0; i < ladder + snake; i++) {
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	
+		q = new LinkedList<int[]>();
+		link = new int[N+M][2];
+		visited = new boolean[101];
+		
+		for(int i =0;i<N+M;i++) {
 			st = new StringTokenizer(br.readLine());
-			start = Integer.parseInt(st.nextToken());
-			end = Integer.parseInt(st.nextToken());
-			map[start] = end;
-		}
-
-		bfs(1);
-	}
-
-	public static void bfs(int idx) {
-
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		queue.offer(idx);
-		visit[idx] = 0;
-		
-		int k;
-		
-		while(!queue.isEmpty()) {
-			
-			k = queue.poll();
-			
-			for(int i =1;i<=6;i++) {
-				
-				if(visit[map[k+i]] == 0) {
-					visit[map[k+i]] = visit[map[k]]+1;
-					queue.offer(map[k+i]);
-				}
-				
-				if(map[k+i]== 100) {
-					System.out.println(visit[100]);
-					return;
-				}
+			for(int j =0;j<2;j++) {
+				link[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		bfs();
+	}
+	
+	public static void bfs() {
+		q.add(new int[] {1,0}); // 0 : 위치 | 1 : 비용
+		visited[1] = true;
+		while(!q.isEmpty()) {
+			int[] k = q.poll();	
+			
+			if(k[0]==100) {
+				System.out.println(k[1]);
+				return;
+			}
+			
+			out: for(int i =1;i<=6;i++) {
+				if(k[0]+i > 100 || visited[k[0]+i]) continue;
+				
+				// 사다리 타기
+				for(int[] point : link) {
+					if(point[0]==k[0]+i) {
+						q.add(new int[]{point[1],k[1]+1});	// 0 : 출발 | 1 : 도착
+						visited[point[1]] = true;
+						continue out;
+					}
+				}
+				
+				// 그냥 가기
+				q.add(new int[] {k[0]+i, k[1]+1});
+				visited[k[0]+i] = true;
+			}
+		}
 	}
 }
