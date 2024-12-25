176837b76094cd0da65257d6ad4afb05a31f482e
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-12-26 00:43:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,39 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int N, K;
+	static int[] W, V;
 
-		int N = sc.nextInt();
-		int W = sc.nextInt();
-
-		int[] weights = new int[N + 1];
-		int[] cost = new int[N + 1];
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		W = new int[N + 1];
+		V = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			weights[i] = sc.nextInt();
-			cost[i] = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
 		}
+		int[][] dp = new int[N + 1][K + 1];
 
-		int[][] dp = new int[N + 1][W + 1];
-
-		// 물건을 고려
 		for (int i = 1; i <= N; i++) {
-
-			// 임시 무게
-			for (int w = 0; w <= W; w++) {
-
-				// 내가 고려할 물건의 무게가 임시무게보다 작다면..
-				if (weights[i] <= w) {
-					dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-weights[i]]+cost[i]);
+			for (int w = 0; w <= K; w++) {
+				if (W[i] <= w) {
+					dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - W[i]] + V[i]);
 				} else {
-					// 아니라면 이전까지 고민한게 베스트
-					dp[i][w] = dp[i-1][w];
+					dp[i][w] = dp[i - 1][w];
 				}
 			}
 		}
-		
-		System.out.println(dp[N][W]);
+		System.out.println(dp[N][K]);
 	}
 }
