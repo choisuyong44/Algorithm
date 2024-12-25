902e875189854745e8af21ed80ca9067577a8d8c
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-12-26 00:04:46 +0900

    [Gold II] Title: 가운데를 말해요, Time: 456 ms, Memory: 39268 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N, k;
@@ -14,27 +12,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
-
 		for (int i = 0; i < N; i++) {
 			k = Integer.parseInt(br.readLine());
-			
-            // 최대 힙(min)과 최소 힙(max)에 값 분배
             if (min.isEmpty() || k <= min.peek()) {
                 min.add(k);
-            } else {
+            } 
+            else {
                 max.add(k);
             }
-
-            // 두 큐의 크기 균형 유지
+            
             if (min.size() > max.size() + 1) {
                 max.add(min.poll());
-            } else if (max.size() > min.size()) {
+            } 
+            else if (max.size() > min.size()) {
                 min.add(max.poll());
             }
-			
 			sb.append(min.peek()).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
 }
